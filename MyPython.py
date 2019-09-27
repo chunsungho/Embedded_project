7ece125fab818f47894a330dc31d4706fbab045a
asw91666 <53108353+asw91666@users.noreply.github.com>
2019-09-27 19:36:18 +0900

    Update MyPython.py

--- MyPython.py
+++ MyPython.py
@@ -1,3 +1,7 @@
+# 소스 실행후 바로 재 실행이 안되면 serv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 추가
+# 파일입출력용 파일 생성후 파일안에 뭐라도 적어놔야함
+# 핸드폰과 동일한 와이파이 사용해야함
+# 본 파이썬용 서버소스 사용시 노트북의 ip주소 확인후 HOST에 저장
 
 import socket
 import os
@@ -5,19 +9,22 @@ import sys
 
 #내집ip =172.30.1.18
 #bestbaram = 192.168.0.144
-HOST = '192.168.0.138'
-
+#HOST = '192.168.0.138'
+HOST = '121.129.63.44'
 PORT = 7800
 
 ADDR = (HOST,PORT)
 
 BUFSIZE = 4096
 
-videofile = "C:\kot.txt"
-filename = "C:\kot.txt"
+#videofile = "C:\kot.txt"
+filename = "/home/chun/PycharmProjects/Embedded_AnsimCamera/file"
 
 
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+
 
     #Bind Socket
 
@@ -48,8 +55,6 @@ f.flush()
 
 f.close()
 
-
-    #Close the Socket
 
 print ('finished writing file')
 
